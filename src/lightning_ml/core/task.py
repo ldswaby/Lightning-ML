524c2e8e23eb7a3bc9d5e860bd57d8e164da718b
Luke Swaby <lukeswabypetts@gmail.com>
2025-06-05 16:45:33 +0100

    More utility methods added to base `Task` class

--- src/lightning_ml/core/task.py
+++ src/lightning_ml/core/task.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from typing import Any, Optional
 
 import pytorch_lightning as pl
-from torch import nn
+from torch import Tensor, nn
 from torch.optim.lr_scheduler import _LRScheduler
 from torch.optim.optimizer import Optimizer
 from torchmetrics import MetricCollection
 
 
-class Task(pl.LightningModule):
+class Task(pl.LightningModule, ABC):
     """
     Build all the ML project components
     TODO: have this have a fixed __init__ that builds all componnets from
@@ -23,15 +24,15 @@ class Task(pl.LightningModule):
         data: pl.LightningDataModule,
         model: nn.Module,
         criterion: nn.Module,
-        metrics: MetricCollection,
         optimizer: Optimizer,
+        metrics: Optional[dict[str, MetricCollection]] = None,
         scheduler: Optional[_LRScheduler] = None,
     ) -> None:
         super().__init__()
         self.data = data
         self.model = model
         self.criterion = criterion
-        self.metrics = metrics
+        self.metrics = metrics or {}
         self.optimizer = optimizer
         self.scheduler = scheduler
 
@@ -45,3 +46,22 @@ class Task(pl.LightningModule):
         """
         # TODO: what about scheduler?
         return self.optimizer
+
+    def on_train_epoch_start(self) -> None:
+        """Resets train metrics per epoch"""
+        if "train" in self.metrics:
+            self.metrics["train"].reset()
+
+    def on_validation_epoch_start(self) -> None:
+        """Resets val metrics per epoch"""
+        if "val" in self.metrics:
+            self.metrics["val"].reset()
+
+    def on_test_epoch_start(self) -> None:
+        """Resets test metrics per epoch"""
+        if "test" in self.metrics:
+            self.metrics["test"].reset()
+
+    @abstractmethod
+    def training_step(self, batch, batch_idx: int) -> Tensor:
+        """Task-specific training logic. Must return loss tensor"""
