dc5270de0f05ff5c383c46e804f366cf0c934f72
Luke Swaby <lukeswabypetts@gmail.com>
2025-06-08 03:12:02 +0100

    sandbox cleanup

--- sandbox.py
+++ sandbox.py
@@ -3,23 +3,7 @@ from pytorch_lightning import Trainer
 from src.lightning_ml.core import Learner, PredictorMixin
 from src.lightning_ml.learners import Supervised
 from src.lightning_ml.predictors import Classification
-
-student = Supervised.with_predictor(Classification)
-
-
-# def bind(learner: type[Learner], predictor: type[PredictorMixin]):
-#     """
-#     TODO: perhaps this should be a `Project` class itself, so it can wrap all the obove with additional logic such as running Task.fit() under Project.train() etc, and use all the correct internal variables
-
-
-#     Dynamically create a new LightningModule that inherits training logic
-#     from `problem_cls` and inference logic from `task_mixin`.
-#     """
-#     name = f"{learner.__name__}{predictor.__name__}"
-#     return type(name, (predictor, learner), {})
-
-
-# SupervisedClassification = bind(Supervised, Classification)
+from src.lightning_ml.utils import bind_classes
 
 # #############
 # model = NotImplemented
@@ -28,19 +12,29 @@ student = Supervised.with_predictor(Classification)
 # criterion = NotImplemented
 # metrics = NotImplemented
 # scheduler = NotImplemented
-
 # #############
 
-# task = SupervisedClassification(
-#     model=model,
-#     optimizer=optimizer,
-#     data=data,
-#     criterion=crtierion,
-#     metrics=metrics,
-#     scheduler=None,  # TODO
-# )
+student = Supervised.with_predictor(
+    Classification,
+    model=model,
+    optimizer=optimizer,
+    data=data,
+    criterion=crtierion,
+    metrics=metrics,
+    scheduler=None,
+)
 
-# trainer = Trainer(limit_train_batches=100, max_epochs=1)
-# trainer.fit(model=model, train_dataloaders=data.train)
+student = bind_classes(Supervised, Classification)(
+    model=model,
+    optimizer=optimizer,
+    data=data,
+    criterion=crtierion,
+    metrics=metrics,
+    scheduler=None,  # TODO
+)
 
-# # should it not then be task.train()? I.e. should one of my objects not already contain a task object?
+# QUESTION: I don't like either of the above.
+# Why can't I define:
+# 1. Learner (with optionally combined PredictorMixin to make a 'student' - learner + optinal predictor)
+# 3. Trainer (pytorch_lighting.Trainer)
+# 4. Project object which combines all the above
