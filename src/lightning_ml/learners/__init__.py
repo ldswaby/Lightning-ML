8209c93f8d9700255943738b694e326ea4f1a058
Luke Swaby <64860440+ldswaby@users.noreply.github.com>
2025-06-20 20:25:19 +0100

    Add SemiSupervised learner

--- src/lightning_ml/learners/__init__.py
+++ src/lightning_ml/learners/__init__.py
@@ -2,13 +2,14 @@
 
 from __future__ import annotations
 
-from .contrastive import *
+from .contrastive import Contrastive
 from .supervised import Supervised
 from .unsupervised import Unsupervised
+from .semi_supervised import SemiSupervised
 
 __all__ = [
     "Supervised",
     "Unsupervised",
-    "ContrastiveSupervised",
-    "ContrastiveUnsupervised",
+    "SemiSupervised",
+    "Contrastive",
 ]
