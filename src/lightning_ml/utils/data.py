2119df6282d1911028a631a1b6ee007458a8c003
Luke Swaby <lukeswabypetts@gmail.com>
2025-07-02 13:28:56 +0100

    Returns generator instead of list to preserve memory

--- src/lightning_ml/utils/data.py
+++ src/lightning_ml/utils/data.py
@@ -5,7 +5,7 @@ This module provides a function to convert sklearn cross-validation splits
 into torch.utils.data.Subset objects for train and validation.
 """
 
-from typing import Dict, List
+from typing import Dict, Iterator
 
 from sklearn.model_selection._split import BaseCrossValidator
 from torch.utils.data import Subset
@@ -15,7 +15,7 @@ from ..core import BaseDataset
 
 def validation_split(
     dataset: BaseDataset, cv: BaseCrossValidator
-) -> List[Dict[str, Subset]]:
+) -> Iterator[Dict[str, Subset]]:
     """Split a PyTorch Dataset into train and validation subsets.
 
     TODO: make support groups?
@@ -32,11 +32,8 @@ def validation_split(
     """
     n_samples = len(dataset)
     y = getattr(dataset, "targets", None)  # for LabelledDatasetBase subclasses
-    splits: List[Dict[str, Subset]] = []
 
     for train_idx, val_idx in cv.split(range(n_samples), y):
         train_subset = Subset(dataset, train_idx)
         val_subset = Subset(dataset, val_idx)
-        splits.append({"train": train_subset, "val": val_subset})
-
-    return splits
+        yield {"train": train_subset, "val": val_subset}
