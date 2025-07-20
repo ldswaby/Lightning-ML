b750d8f7d41b8a05a1655e19afc7999843345b93
Luke Swaby <64860440+ldswaby@users.noreply.github.com>
2025-07-20 19:46:23 +0100

    Add modular registry initialization

---
+++ src/lightning_ml/core/data/dataset.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper forwarding to :mod:`lightning_ml.core.abstract.dataset`."""
+
+from ..abstract.dataset import *  # noqa: F401,F403
---
+++ src/lightning_ml/core/data/loader.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper forwarding to :mod:`lightning_ml.core.abstract.loader`."""
+
+from ..abstract.loader import *  # noqa: F401,F403
